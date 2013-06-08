875f3329f14ac66a33b48b373a6dad64f9475c07
Vitaliy Medvedev <medvedev.vs@gmail.com>
2013-06-09 02:44:28 +0400

    try deploy to heroku

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 define('APP_WEB_PATH', '');
 define('APP_PATH', $_SERVER['DOCUMENT_ROOT'] . APP_WEB_PATH);
-define('APP_DIR', dirname(__FILE__) . '\\');
+define('APP_DIR', dirname(__FILE__) . '/');
 
 function my_autoloader($class)
 {
